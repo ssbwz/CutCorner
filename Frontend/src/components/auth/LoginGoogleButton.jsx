c28f8ec4633bdd5ce61e42eca6962d071af4c2e5
i488364 <i488364@fhict.nl>
2023-06-29 21:32:37 +0200

    Adding the navbar

--- Frontend/src/components/auth/LoginGoogleButton.jsx
+++ Frontend/src/components/auth/LoginGoogleButton.jsx
@@ -11,11 +11,17 @@ function LoginGoogleButton(props) {
         navigate('/myProfile');
     }
 
-    const onFailure = () => {
+    function onSignIn(googleUser) {
+        var profile = googleUser.getBasicProfile();
+        console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+        console.log('Name: ' + profile.getName());
+        console.log('Image URL: ' + profile.getImageUrl());
+        console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 
     return <>
-</>
+      <div class="g-signin2" data-onsuccess="onSignIn"></div>
+    </>
 }
 
 export default LoginGoogleButton;
