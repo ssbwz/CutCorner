6ca9dd34e3976589047915e9ce66a0a9ddedb909
i488364 <i488364@fhict.nl>
2023-07-05 19:25:09 +0200

    Adding NotFoundPage

---
+++ Frontend/src/pages/NotFoundPage.jsx
@@ -0,0 +1,10 @@
+function NotFound() {
+    return <>
+        <div style={{height: '89.1vh'}} class="container">
+            <p class="fs-1">404</p>
+            <h5>We can't find the page you are looking for.</h5>
+        </div>
+    </>
+}
+
+export default NotFound;
